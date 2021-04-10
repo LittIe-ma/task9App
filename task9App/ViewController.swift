c08edcf8426e10a7406c2ff5937a7fc15e2fe151
yasu1125 <v22.m1125@gmail.com>
2021-04-10 17:39:10 +0900

    Initial Commit

---
+++ task9App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  task9App
+//
+//  Created by yasudamasato on 2021/04/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
